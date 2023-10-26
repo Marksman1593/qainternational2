65d88bf3d31fd016895200fbfa5dc3fdc6471d9f
Bilal Azfar <bilalazfar1593@gmail.com>
2023-10-26 14:41:39 +0500

    Contact Us front end Done

--- JS/mover.js
+++ JS/mover.js
@@ -5,16 +5,63 @@ const observer = new IntersectionObserver(handleIntersection, {
 
 observer.observe(picsElement);
   
-  function handleIntersection(entries, observer) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("initiate");
-  
-        // setTimeout(() => {
-        //   entry.target.classList.remove("initiate");
-        // }, 1000);
-  
-        observer.unobserve(entry.target);
-      }
-    });
-  }
+function handleIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("initiate");  
+      observer.unobserve(entry.target);
+    }
+  });
+}
+var state = 0;
+
+function expand(){
+  var navbar = document.getElementsByClassName("nav");
+  var contactus = document.getElementById("contactus");
+    
+  let response = expansion().then(result => {
+    if(state==1){
+      contactus.classList.add("expansion");
+    }
+    else{
+      contactus.classList.remove("expansion");
+      // retraction().then(result1 => {
+      //   contactus.style.display = "none";
+      // });
+      // contactus.style.transform = "translateY(100%)"
+    }
+  });
+}
+
+function expansion(){
+  return new Promise(function(resolve, reject) {
+    var navbar = document.getElementsByClassName("nav");
+    nav = navbar[0];
+    if(!nav.classList.contains("expanded") && state == 0){
+      nav.classList.add("expanded");
+      state = 1;
+    }
+    else{
+      nav.classList.remove("expanded");
+      state = 0;
+    }
+    setTimeout(function() {
+      console.log('Task is complete.');
+      resolve();
+    }, 1000);
+  });
+}
+
+function retraction(){
+  return new Promise(function(resolve, reject){
+    var contactus = document.getElementById("contactus");
+    contactus.style.transform = "translateY(100%)"
+    setTimeout(function(){
+      console.log("Retracted");
+      resolve();
+    }, 1000);
+
+  });
+}
+
+
